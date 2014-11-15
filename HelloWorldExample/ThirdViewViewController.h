5bd2d33e1c80d467667b9fb5022ec72c9c17fcba
Thien NH <toronguyen2014@gmail.com>
2014-11-15 18:33:44 +0700

    Add third view

---
+++ HelloWorldExample/ThirdViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewViewController.h
+//  HelloWorldExample
+//
+//  Created by Toro on 11/15/14.
+//  Copyright (c) 2014 VINControl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewViewController : UIViewController
+
+@end
